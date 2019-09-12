83bb150dc109d0c6d4febbdb89c8612ba2867fa2
federico <federico.rieckhof@gmail.com>
2019-09-12 21:51:49 +0200

    minor refactoring

--- format.sh
+++ format.sh
@@ -4,7 +4,6 @@ then
   find ./src ./test -iname '*.h' -o -iname '*.cpp' | xargs clang-format-7 -i -style=Chromium
 else
   command=`find ./src ./test -iname '*.h' -o -iname '*.cpp' | xargs clang-format-7 -output-replacements-xml | grep -c "<replacement "`
-  #echo ${command[@]}
   if [ "${command}" -gt 0 ];
   then
     echo "${command} unsresolved issue(s). Call format.sh -d to resolve them."
